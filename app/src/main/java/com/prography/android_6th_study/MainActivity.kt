ab8aad37e8e0f6ecc9c64127d9444a950b0b695d
조연우 <wooisso@naver.com>
2020-04-03 23:12:35 +0900

    Initial commit

---
+++ app/src/main/java/com/prography/android_6th_study/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.prography.android_6th_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
